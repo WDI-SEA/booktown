2e02907385d27649ed4d3829eae17e072309d5aa
Scott <scrosehart@gmail.com>
2018-02-14 16:54:26 -0800

    finished yo

---
+++ answers.sql
@@ -0,0 +1,91 @@
+-- Order
+--
+-- Find all subjects sorted by subject
+--
+--
+-- Find all subjects sorted by location
+--
+--
+-- Where
+-- 
+-- Find the book "Little Women"
+--
+--
+-- Find all books containing the word "Python"
+--
+--
+-- Find all subjects with the location "Main St" sort them by subject
+--
+--
+-- Joins
+--
+-- Find all books about Computers and list ONLY the book titles
+--
+--
+-- Find all books and display a result table with ONLY the following columns
+--
+--
+-- Book title
+--
+--
+-- Author's first name
+--
+--
+-- Author's last name
+--
+--
+-- Book subject
+--
+--
+-- Find all books that are listed in the stock table
+--
+--
+-- Sort them by retail price (most expensive first)
+--
+--
+-- Display ONLY: title and price
+--
+--
+-- Find the book "Dune" and display ONLY the following columns
+--
+--
+-- Book title
+--
+--
+-- ISBN number
+--
+--
+-- Publisher name
+--
+--
+-- Retail price
+--
+--
+-- Find all shipments sorted by ship date display a result table with ONLY the following columns:
+--
+--
+-- Customer first name
+--
+--
+-- Customer last name
+--
+--
+-- ship date
+--
+--
+-- book title
+--
+--
+-- Grouping and Counting
+--
+-- Get the COUNT of all books
+--
+--
+-- Get the COUNT of all Locations
+--
+--
+-- Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+--
+--
+-- List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+--
