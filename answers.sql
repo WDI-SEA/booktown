85cf0bc156bf997eef777de30596eb3ed02a675e
Zakir Butte <zakir.butte@gmailcom>
2018-02-15 09:40:22 -0800

    fixed some answers

--- answers.sql
+++ answers.sql
@@ -14,7 +14,7 @@ WHERE title = 'Little Women';
 -- Find all books containing the word "Python"
 
 SELECT * FROM books
-WHERE title LIKE '%Python%';
+WHERE title LIKE 'Python%';
 
 -- Find all subjects with the location "Main St" sort them by subject
 SELECT subject FROM subjects
@@ -23,7 +23,7 @@ ORDER BY subject;
 
 -- Find all books about Computers and list ONLY the book titles
 
-SELECT title
+SELECT books.title
 FROM books
 JOIN subjects ON subjects.id = books.subject_id
 WHERE subjects.subject = 'Computers';
@@ -59,7 +59,8 @@ SELECT books.title, editions.isbn, publishers.name, stock.retail
 FROM stock
 JOIN editions ON editions.isbn = stock.isbn
 JOIN books ON books.id = editions.book_id
-JOIN publishers ON publishers.id = editions.publisher_id;
+JOIN publishers ON publishers.id = editions.publisher_id
+WHERE book.title = 'DUNE';
 
 
 -- Find all shipments sorted by ship date display a result table with ONLY the following columns:
@@ -72,7 +73,8 @@ SELECT c.first_name, c.last_name, s.ship_date, b.title
 FROM shipments s
 JOIN customers c ON c.id = s.customer_id
 JOIN editions e ON e.isbn = s.isbn
-JOIN books b ON b.id = e.book_id;
+JOIN books b ON b.id = e.book_id
+ORDER BY s.ship_date;
 
 
 -- Get the COUNT of all books
@@ -83,14 +85,14 @@ SELECT COUNT(*) FROM books;
 SELECT COUNT(DISTINCT location) FROM subjects;
 
 -- Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
-SELECT DISTINCT location, COUNT(location)
+SELECT location, COUNT(location)
 FROM subjects
 GROUP BY location;
 
 
 -- List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
 
-SELECT b.id, b.title, MAX(e.edition) AS Editions_Available
+SELECT b.id, b.title, COUNT(DISTINCT e.edition) AS Editions_Available
 FROM editions e
 JOIN books b ON b.id = e.book_id
 GROUP BY b.id;
