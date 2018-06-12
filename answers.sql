c8568feee65bc30c4827413400dc9e6c9cf26615
Matt Carver <mattcarver.wd@gmail.com>
2018-06-12 08:54:46 -0700

    worked through more join

--- answers.sql
+++ answers.sql
@@ -92,25 +92,85 @@ booktown=# select book_backup.title from book_backup full outer join subjects on
 	* Author's first name
 	* Author's last name
 	* Book subject
+  select book_backup.title, book_backup.subject_id, authors.first_name, authors.last_name from book_backup inner join authors on book_backup.author_id=authors.id;
+
 * Find all books that are listed in the stock table
 	* Sort them by retail price (most expensive first)
 	* Display ONLY: title and price
+  booktown=# select title, retail from books join editions on books.id=editions.book_id join stock on editions.isbn=stock.isbn order by retail desc;
+              title            | retail
+  -----------------------------+--------
+   2001: A Space Odyssey       |  46.95
+   Dune                        |  45.95
+   The Shining                 |  36.95
+   The Cat in the Hat          |  32.95
+   Goodnight Moon              |  28.95
+   The Shining                 |  28.95
+   Dynamic Anatomy             |  28.95
+   The Tell-Tale Heart         |  24.95
+   The Velveteen Rabbit        |  24.95
+   The Cat in the Hat          |  23.95
+   Franklin in the Dark        |  23.95
+   Little Women                |  23.95
+   2001: A Space Odyssey       |  22.95
+   The Tell-Tale Heart         |  21.95
+   Dune                        |  21.95
+   Bartholomew and the Oobleck |  16.95
+  (16 rows)
+
+
 * Find the book "Dune" and display ONLY the following columns
 	* Book title
 	* ISBN number
 	* Publisher name
 	* Retail price
+
+select title, editions.isbn, name, retail from books join editions on books.id=editions.book_id
+  join publishers on editions.publisher_id=publishers.id join stock on editions.isbn=stock.isbn where title like '%Dune%';
+
+editions
+
 * Find all shipments sorted by ship date display a result table with ONLY the following columns:
 	* Customer first name
 	* Customer last name
 	* ship date
 	* book title
 
+select first_name, last_name, ship_date, title from customers join shipments on
+
+
 ### Grouping and Counting
 
 * Get the COUNT of all books
+booktown=# select count(*) from books;
+ count
+-------
+    15
+
 * Get the COUNT of all Locations
+booktown=# select count(location) from subjects;
+ count
+-------
+    15
+(1 row)
+
 * Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+booktown=# select count(location), location from subjects group by location;
+ count |     location
+-------+------------------
+     0 |
+     1 | Sunset Dr
+     1 | Kids Ct
+     2 | Black Raven Dr
+     2 | Creativity St
+     2 | Academic Rd
+     4 | Main St
+     3 | Productivity Ave
+(8 rows)
+
+
 * List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+
+select book_id, title, count(editions) from books join editions on editions.book_id=books.id;
 
 #### YAY! You're done!!
