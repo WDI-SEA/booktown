305745ec8cab8bb0de0b4595ff87ca639ff1bc68
Ari <gryphon150@gmail.com>
2019-10-09 17:58:15 -0700

    finished all WHERE questions

--- answers.sql
+++ answers.sql
@@ -1,5 +1,15 @@
--- answer 1 
+-- ORDER: answer 1 
 SELECT subject FROM subjects;
 
--- answer 2
+-- ORDER: answer 2
 SELECT location FROM subjects;
+
+-- WHERE: answer 1 
+SELECT title FROM books WHERE title='Little Women';
+
+-- WHERE: answer 2
+SELECT title FROM books WHERE title LIKE '%Python%'
+
+-- WHERE: answer 3 
+SELECT subject, location FROM subjects WHERE location='Main St' ORDER BY subject DESC;
+
