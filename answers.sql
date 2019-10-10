99ea251a435095892c8e0d01b49819484d40b6aa
sross9845 <sean.ross.985@gmail.com>
2019-10-10 09:38:01 -0700

    fixed a few things and used aliases

--- answers.sql
+++ answers.sql
@@ -11,28 +11,28 @@ SELECT * FROM books WHERE title LIKE '%Python%';
 -- Find all subjects with the location "Main St" sort them by subject
 SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 -- Find all books about Computers and list ONLY the book titles
-SELECT title FROM books JOIN subjects ON subjects.id = books.subject_id WHERE subject = 'Computers';
+SELECT books.title FROM books INNER JOIN subjects ON subjects.id = books.subject_id WHERE subject = 'Computers';
 -- Find all books and display a result table with ONLY the following columns
-SELECT title, authors.first_name, authors.last_name, subjects.subject FROM books JOIN authors ON books.author_id = authors.id JOIN subjects ON books.subject_id = subjects.id;
+SELECT b.title, a.first_name, a.last_name, s.subject FROM books b INNER JOIN authors a ON b.author_id = a.id INNER JOIN subjects s ON b.subject_id = s.id;
 -- Book title
 -- Author's first name
 -- Author's last name
 -- Book subject
 
 -- Find all books that are listed in the stock table
-SELECT books.title, stock.retail FROM books JOIN editions ON books.id = editions.book_id JOIN stock ON editions.isbn = stock.isbn ORDER BY retail DESC;
+SELECT b.title, s.retail FROM books b INNER JOIN editions e ON b.id = e.book_id INNER JOIN stock s ON e.isbn = s.isbn ORDER BY retail DESC;
 -- Sort them by retail price (most expensive first)
 -- Display ONLY: title and price
 
 -- Find the book "Dune" and display ONLY the following columns
-SELECT books.title, editions.isbn, publishers.name, stock.retail FROM books JOIN editions ON books.id = editions.book_id JOIN stock ON editions.isbn = stock.isbn JOIN publishers ON editions.publisher_id = publishers.id WHERE books.title = 'Dune';
+SELECT b.title, e.isbn, p.name, s.retail FROM books b INNER JOIN editions e ON b.id = e.book_id INNER JOIN stock s ON e.isbn = s.isbn INNER JOIN publishers p ON e.publisher_id = p.id WHERE b.title = 'Dune';
 -- Book title
 -- ISBN number
 -- Publisher name
 -- Retail price
 
 -- Find all shipments sorted by ship date display a result table with ONLY the following columns:
-SELECT customers.first_name, customers.last_name, books.title, shipments.ship_date FROM books JOIN editions ON books.id = editions.book_id JOIN shipments ON editions.isbn = shipments.isbn JOIN customers ON shipments.customer_id = customers.id;
+SELECT c.first_name, c.last_name, b.title, s.ship_date FROM books b INNER JOIN editions e ON b.id = e.book_id INNER JOIN shipments s ON e.isbn = s.isbn INNER JOIN customers c ON s.customer_id = c.id ORDER BY shipments.ship_date DESC;
 -- Customer first name
 -- Customer last name
 -- ship date
@@ -44,8 +44,8 @@ SELECT count(*) FROM books;
 -- Get the COUNT of all Locations
 SELECT COUNT(subjects.location) FROM subjects;
 -- Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
-SELECT COUNT(subjects.location), subjects.location  FROM subjects GROUP BY subjects.location;
+SELECT COUNT(s.location), s.location  FROM subjects s GROUP BY s.location;
 -- List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
-SELECT books.id,books.title, COUNT(editions.edition) AS number_of_editions FROM books JOIN editions ON books.id = editions.book_id GROUP BY books.id;
+SELECT b.id,b.title, COUNT(e.edition) AS number_of_editions FROM books b INNER JOIN editions e ON b.id = e.book_id GROUP BY b.id;
 --or(incase i did this wrong)
-SELECT books.id, books.title, editions.edition FROM books JOIN editions ON editions.book_id = books.id GROUP BY editions.edition, books.title, books.id;
+SELECT b.id, b.title, e.edition FROM books b INNER JOIN editions e ON e.book_id = b.id GROUP BY e.edition, b.title, b.id;
