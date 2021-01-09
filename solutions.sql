50d580d769e609f69c7d6903546520676110637c
Your Name <you@example.com>
2021-01-08 17:19:50 -0800

    Got up to and including problem 8.

--- solutions.sql
+++ solutions.sql
@@ -1,3 +1,16 @@
+-- Enter preSQL
+psql
+-- List databases.
+\l 
+-- Connect / cd to a database.
+\connect booktown
+-- Display all the tables within that database. 
+\d
+-- Display subjects table.
+\d subjects
+-- Quit preSQL
+\q
+
 -- ### Order
 -- 1. Find all subjects sorted by subject
 SELECT * FROM subjects ORDER BY subject ASC;
@@ -6,21 +19,52 @@ SELECT * FROM subjects ORDER BY location ASC;
 
 -- ### Where
 -- 3. Find the book "Little Women"
--- 4. Find all books containing the word "Python"
+SELECT title FROM books WHERE title='Little Women';
+SELECT * FROM books WHERE title='Little Women';
+-- 4. Find all books containing the word "Python";
+SELECT * FROM books WHERE title LIKE 'Python%' OR title LIKE '%Python' OR title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT * FROM subjects WHERE location = 'Main St';
 
 -- ### Joins
-
 -- 6. Find all books about Computers and list ONLY the book titles
+books has id, title, author_id, subject_id
+subjects has id, subject, location
+According to subjects, 'Computers' has an id of 4. 
+
+SELECT b.title 
+FROM books b INNER JOIN subjects s
+ON b.subject_id = s.id
+WHERE s.subject = 'Computers';
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+-- How to join multiple tables: https://learnsql.com/blog/how-to-join-3-tables-or-more-in-sql/
+books.id, books.title, books.author_id, books.subject_id
+authors.id, authors.last_name, authors.first_name
+subjects.id, subjects.subject, subject.location
+
+SELECT b.title, a.first_name, a.last_name, s.subject
+FROM books b INNER JOIN authors a 
+ON b.author_id = a.id 
+INNER JOIN subjects s
+ON b.subject_id = s.id;
+
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+stock.isbn, stock.cost, stock.retail, stock.stock
+editions.isbn, editions.book_id
+books.id, books.title
+
+SELECT b.title, s.cost
+FROM stock s INNER JOIN editions e
+ON s.isbn = e.isbn 
+INNER JOIN books b
+ON e.book_id = b.id;
+
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
@@ -31,6 +75,7 @@ SELECT * FROM subjects ORDER BY location ASC;
 -- 	* Customer last name
 -- 	* ship date
 -- 	* book title
+
 
 -- ### Grouping and Counting
 
