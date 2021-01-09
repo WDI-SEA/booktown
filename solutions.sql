7447791b15fd1837c6390788a8372bced2a4d6d3
wkoslik <wkoslik@gmail.com>
2021-01-09 09:40:28 -0500

    first five questions

--- solutions.sql
+++ solutions.sql
@@ -1,11 +1,47 @@
+                     List of relations
+ Schema |         Name          |   Type   |     Owner     
+--------+-----------------------+----------+---------------
+ public | alternate_stock       | table    | whitneykoslik
+ public | authors               | table    | whitneykoslik
+ public | book_backup           | table    | whitneykoslik
+ public | book_ids              | sequence | whitneykoslik
+ public | book_queue            | table    | whitneykoslik
+ public | books                 | table    | whitneykoslik
+ public | customers             | table    | whitneykoslik
+ public | daily_inventory       | table    | whitneykoslik
+ public | distinguished_authors | table    | whitneykoslik
+ public | editions              | table    | whitneykoslik
+ public | employees             | table    | whitneykoslik
+ public | favorite_authors      | table    | whitneykoslik
+ public | favorite_books        | table    | whitneykoslik
+ public | money_example         | table    | whitneykoslik
+ public | my_list               | table    | whitneykoslik
+ public | numeric_values        | table    | whitneykoslik
+ public | publishers            | table    | whitneykoslik
+ public | schedules             | table    | whitneykoslik
+ public | shipments             | table    | whitneykoslik
+ public | states                | table    | whitneykoslik
+ public | stock                 | table    | whitneykoslik
+ public | stock_backup          | table    | whitneykoslik
+ public | subject_ids           | sequence | whitneykoslik
+ public | subjects              | table    | whitneykoslik
+ public | text_sorting          | table    | whitneykoslik
+
+
 -- ### Order
 -- 1. Find all subjects sorted by subject
+    SELECT subject FROM subjects ORDER BY subject;
+
 -- 2. Find all subjects sorted by location
+    SELECT subject, location FROM subjects ORDER BY location;
 
 -- ### Where
 -- 3. Find the book "Little Women"
+    SELECT * FROM books WHERE title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+    SELECT * FROM books WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
+    SELECT subject, location FROM subjects WHERE location = 'Main St' ORDER BY subject;
 
 
 -- ### Joins
