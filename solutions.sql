d98212d11bd3b5d582f86c3c89882c77c24b8e02
tkadima <kadima.t@husky.neu.edu>
2021-01-10 13:36:28 -0500

    got problem four wrapped up

--- solutions.sql
+++ solutions.sql
@@ -85,4 +85,11 @@ ANSWERS
 --------------
  Little Women
 
+4. booktown=# SELECT * FROM books WHERE title LIKE '%Python%';
+  id   |       title        | author_id | subject_id 
+-------+--------------------+-----------+------------
+ 41473 | Programming Python |      7805 |          4
+ 41477 | Learning Python    |      7805 |          4
+
+5.
 
