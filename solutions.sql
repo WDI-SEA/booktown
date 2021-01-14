52d0acd043633ab9069a6606a101ebbb564f3054
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-01-14 13:36:17 -0500

    need to work on joins

--- solutions.sql
+++ solutions.sql
@@ -1,16 +1,70 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+ id |     subject      |     location     
+----+------------------+------------------
+  0 | Arts             | Creativity St
+  1 | Business         | Productivity Ave
+  2 | Childrens Books | Kids Ct
+  3 | Classics         | Academic Rd
+  4 | Computers        | Productivity Ave
+  5 | Cooking          | Creativity St
+  6 | Drama            | Main St
+  7 | Entertainment    | Main St
+  8 | History          | Academic Rd
+  9 | Horror           | Black Raven Dr
+ 10 | Mystery          | Black Raven Dr
+ 11 | Poetry           | Sunset Dr
+ 12 | Religion         | 
+ 13 | Romance          | Main St
+ 14 | Science          | Productivity Ave
+ 15 | Science Fiction  | Main St
 -- 2. Find all subjects sorted by location
+     subject      
+------------------
+ History
+ Classics
+ Mystery
+ Horror
+ Arts
+ Cooking
+ Childrens Books
+ Entertainment
+ Drama
+ Romance
+ Science Fiction
+ Science
+ Computers
+ Business
+ Poetry
+ Religion
 
 -- ### Where
 -- 3. Find the book "Little Women"
+    title     
+--------------
+ Little Women
 -- 4. Find all books containing the word "Python"
+       title        
+--------------------
+ Programming Python
+ Learning Python
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+     subject     
+-----------------
+ Drama
+ Entertainment
+ Romance
+ Science Fiction
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+        title         
+----------------------
+ Programming Python
+ Learning Python
+ Perl Cookbook
+ Practical PostgreSQL
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
@@ -33,6 +87,29 @@
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+ count 
+-------
+    15
 -- 12. Get the COUNT of all Locations
+ count 
+-------
+    15
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+ count 
+-------
+    17
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+  id   |            title            | count 
+-------+-----------------------------+-------
+  2038 | Dynamic Anatomy             |     1
+ 25908 | Franklin in the Dark        |     1
+  7808 | The Shining                 |     2
+  4267 | 2001: A Space Odyssey       |     2
+ 41473 | Programming Python          |     1
+  1234 | The Velveteen Rabbit        |     1
+  4513 | Dune                        |     2
+  1608 | The Cat in the Hat          |     2
+   190 | Little Women                |     1
+  1501 | Goodnight Moon              |     1
+   156 | The Tell-Tale Heart         |     2
+  1590 | Bartholomew and the Oobleck |     1
