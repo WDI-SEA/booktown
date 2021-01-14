ae7b229819d8c35939565b9b0c6bab82a91f7cc2
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-01-14 12:37:26 -0600

    final booktown

--- solutions.sql
+++ solutions.sql
@@ -38,13 +38,24 @@
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
 
 
--- SELECT * FROM subjects
--- SELECT * FROM subjects ORDER BY location;
--- SELECT * FROM books    SELECT * FROM books WHERE id = '190'
--- SELECT * FROM subjects WHERE location ='Main St' ORDER BY subject;
--- SELECT * FROM books WHERE subject_id = '4';
--- SELECT books.title, authors.first_name, authors.last_name, subjects.subject
+-- 1.SELECT * FROM subjects
+-- 2.SELECT * FROM subjects ORDER BY location;
+-- 3.SELECT * FROM books    SELECT * FROM books WHERE id = '190'
+-- 4.SELECT * FROM books WHERE title LIKE '%Python';
+-- 5.SELECT * FROM subjects WHERE location ='Main St' ORDER BY subject;
+-- 6.SELECT * FROM books WHERE subject_id = '4';
+-- 7.SELECT books.title, authors.first_name, authors.last_name, subjects.subject
 -- booktown-# FROM authors
 -- booktown-# JOIN books ON books.author_id = authors.id
 -- booktown-# JOIN subjects ON subjects.id = books.subject_id;
--- SELECT * FROM stock ORDER BY retail DESC;     
+-- 8.SELECT * FROM stock ORDER BY retail DESC;
+--  SELECT books.title, stock.retail FROM books INNER JOIN editions ON books.id=editions.book_id INNER JOIN
+-- booktown-# stock ON stock.isbn=editions.isbn ORDER BY stock.retail DESC;     
+-- 9.SELECT b.title, s.isbn, p.name, s.retail FROM books b JOIN editions e ON b.id=e.book_id JOIN stock s
+-- booktown-# ON s.isbn=e.isbn JOIN publishers p ON e.publisher_id=p.id WHERE b.title = 'Dune';
+-- 10.SELECT c.first_name, c.last_name, s.ship_date, b.title FROM customers c JOIN shipments s ON
+-- booktown-# c.id=s.customer_id JOIN editions e ON e.isbn=s.isbn JOIN books b ON b.id=e.book_id ORDER BY s.ship_date DESC;
+-- 11. SELECT COUNT(*) FROM books;
+-- 12.SELECT COUNT(location) FROM subjects;
+-- 13.  SELECT location, COUNT(location) FROM subjects GROUP BY location;
+-- 14. SELECT b.id, b.title, COUNT(e.edition) FROM books b JOIN editions e ON b.id=e.book_id GROUP BY b.id;
