84f8a9f67984c06abbb40de1281a1b6523a99a53
devinrbopp <devinrbopp@gmail.com>
2021-11-01 19:47:05 -0600

    finished assignment

--- solutions.sql
+++ solutions.sql
@@ -117,20 +117,133 @@ booktown=# SELECT b.title, a.first_name, a.last_name, s.subject FROM books b LEF
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+booktown=# SELECT b.title, s.retail FROM stock s LEFT JOIN editions e ON e.isbn = s.isbn LEFT JOIN books b ON e.book_id=b.id ORDER BY s.retail DESC;
+--             title            | retail 
+-- -----------------------------+--------
+--  2001: A Space Odyssey       |  46.95
+--  Dune                        |  45.95
+--  The Shining                 |  36.95
+--  The Cat in the Hat          |  32.95
+--  Goodnight Moon              |  28.95
+--  The Shining                 |  28.95
+--  Dynamic Anatomy             |  28.95
+--  The Tell-Tale Heart         |  24.95
+--  The Velveteen Rabbit        |  24.95
+--  The Cat in the Hat          |  23.95
+--  Franklin in the Dark        |  23.95
+--  Little Women                |  23.95
+--  2001: A Space Odyssey       |  22.95
+--  The Tell-Tale Heart         |  21.95
+--  Dune                        |  21.95
+--  Bartholomew and the Oobleck |  16.95
+-- (16 rows)
+
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
 -- 	* Publisher name
 -- 	* Retail price
+booktown=# SELECT b.title, e.isbn, p.name, s.retail FROM books b LEFT JOIN editions e ON b.id=e.book_id LEFT JOIN publishers p ON e.publisher_id=p.id LEFT JOIN stock s ON e.isbn=s.isbn WHERE b.title='Dune';
+
+--  title |    isbn    |   name    | retail 
+-- -------+------------+-----------+--------
+--  Dune  | 0441172717 | Ace Books |  21.95
+--  Dune  | 044100590X | Ace Books |  45.95
+-- (2 rows)
+
+
 -- 10. Find all shipments sorted by ship date display a result table with ONLY the following columns:
 -- 	* Customer first name
 -- 	* Customer last name
 -- 	* ship date
 -- 	* book title
 
+booktown=# SELECT c.first_name, c.last_name, s.ship_date, b.title FROM shipments s LEFT JOIN customers c ON s.customer_id=c.id LEFT JOIN editions e ON s.isbn=e.isbn LEFT JOIN books b ON e.book_id = b.id ORDER BY s.ship_date;
+
+--  first_name | last_name |       ship_date        |            title            
+-- ------------+-----------+------------------------+-----------------------------
+--  Owen       | Bollman   | 2001-08-05 10:34:04-06 | Little Women
+--  Laura      | Bennett   | 2001-08-06 08:49:44-06 | Goodnight Moon
+--  Chad       | Allen     | 2001-08-06 10:29:21-06 | The Cat in the Hat
+--  Annie      | Jackson   | 2001-08-06 12:46:36-06 | Bartholomew and the Oobleck
+--  Annie      | Jackson   | 2001-08-07 11:58:36-06 | Bartholomew and the Oobleck
+--  Royce      | Morrill   | 2001-08-07 12:31:57-06 | The Tell-Tale Heart
+--  Christine  | Holloway  | 2001-08-07 12:56:42-06 | 2001: A Space Odyssey
+--  Eric       | Morrill   | 2001-08-07 14:00:48-06 | Little Women
+--  Jonathan   | Anderson  | 2001-08-08 09:36:44-06 | The Shining
+--  Ed         | Gould     | 2001-08-08 10:53:46-06 | The Shining
+--  Annie      | Jackson   | 2001-08-08 11:46:13-06 | Bartholomew and the Oobleck
+--  Dave       | Olson     | 2001-08-09 08:30:07-06 | The Velveteen Rabbit
+--  Wendy      | Black     | 2001-08-09 10:30:46-06 | The Velveteen Rabbit
+--  Rich       | Thomas    | 2001-08-10 08:29:52-06 | Franklin in the Dark
+--  Jean       | Black     | 2001-08-10 09:29:42-06 | The Tell-Tale Heart
+--  Eric       | Morrill   | 2001-08-10 14:47:52-06 | The Cat in the Hat
+--  Annie      | Jackson   | 2001-08-11 10:55:05-06 | Bartholomew and the Oobleck
+--  Richard    | Brown     | 2001-08-11 11:52:34-06 | Goodnight Moon
+--  James      | Williams  | 2001-08-11 14:34:08-06 | The Cat in the Hat
+--  Kate       | Gerdes    | 2001-08-12 09:46:35-06 | Dune
+--  Jean       | Owens     | 2001-08-12 13:09:47-06 | Franklin in the Dark
+--  Owen       | Becker    | 2001-08-12 14:39:22-06 | The Shining
+--  Julie      | Bollman   | 2001-08-13 10:42:10-06 | The Cat in the Hat
+--  Kathy      | Corner    | 2001-08-13 10:47:04-06 | The Cat in the Hat
+--  Tim        | Owens     | 2001-08-14 08:33:47-06 | Dynamic Anatomy
+--  Trevor     | Young     | 2001-08-14 09:42:58-06 | Dune
+--  Chuck      | Brown     | 2001-08-14 11:36:41-06 | The Shining
+--  Adam       | Holloway  | 2001-08-14 14:41:39-06 | The Tell-Tale Heart
+--  Jenny      | King      | 2001-08-14 14:45:51-06 | The Shining
+--  Tammy      | Robinson  | 2001-08-14 14:49:00-06 | Franklin in the Dark
+--  James      | Clark     | 2001-08-15 12:57:40-06 | Goodnight Moon
+
+
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+booktown=# SELECT count(*) FROM books;
+--  count 
+-- -------
+--     15
+-- (1 row)
+
 -- 12. Get the COUNT of all Locations
+booktown=# SELECT count(location) FROM subjects;
+--  count 
+-- -------
+--     15
+-- (1 row)
+
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+booktown=# SELECT location, count(location) FROM subjects GROUP BY location;
+--      location     | count 
+-- ------------------+-------
+--                   |     0
+--  Sunset Dr        |     1
+--  Kids Ct          |     1
+--  Black Raven Dr   |     2
+--  Creativity St    |     2
+--  Academic Rd      |     2
+--  Main St          |     4
+--  Productivity Ave |     3
+-- (8 rows)
+
+
+
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+booktown=# SELECT b.id, b.title, count(e.isbn) FROM books b LEFT JOIN editions e ON e.book_id=b.id GROUP BY b.id;
+--   id   |            title            | count 
+-- -------+-----------------------------+-------
+--  41477 | Learning Python             |     0
+--  25908 | Franklin in the Dark        |     1
+--  41478 | Perl Cookbook               |     0
+--    190 | Little Women                |     1
+--   1501 | Goodnight Moon              |     1
+--    156 | The Tell-Tale Heart         |     2
+--  41472 | Practical PostgreSQL        |     0
+--   2038 | Dynamic Anatomy             |     1
+--   7808 | The Shining                 |     2
+--   4267 | 2001: A Space Odyssey       |     2
+--  41473 | Programming Python          |     1
+--   1234 | The Velveteen Rabbit        |     1
+--   4513 | Dune                        |     2
+--   1608 | The Cat in the Hat          |     2
+--   1590 | Bartholomew and the Oobleck |     1
+-- (15 rows)
