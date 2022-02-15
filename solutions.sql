006ae6efcf03a8593ad9eaf2b4ead072e6a63bab
evankski <evankarwowski@gmail.com>
2022-02-15 10:17:55 -0800

    finishes deliverable

--- solutions.sql
+++ solutions.sql
@@ -1,38 +1,50 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+-- SELECT * FROM subjects;
 -- 2. Find all subjects sorted by location
+--  SELECT subject, location FROM subjects;
 
 -- ### Where
 -- 3. Find the book "Little Women"
+-- SELECT title FROM books WHERE title = 'Little Women';
 -- 4. Find all books containing the word "Python"
+-- SELECT * FROM books WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+-- SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+-- SELECT books.title FROM books JOIN subjects ON books.subject_id = subjects.id WHERE subjects.id = '4';
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+-- SELECT a.last_name, a.first_name, b.title, s.subject FROM authors a JOIN books b ON a.id = b.author_id JOIN subjects s ON s.id = b.subject_id
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+-- SUBJECT b.title FROM books b JOIN editions e ON b.id = e.book_id JOIN stock S ON e.isbn
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
 -- 	* Publisher name
 -- 	* Retail price
+-- SUBJECT b.title, e.isbn, p.name AS publisher_name, s.retail FROM books b JOIN editions e ON b.id = e.book_id
 -- 10. Find all shipments sorted by ship date display a result table with ONLY the following columns:
 -- 	* Customer first name
 -- 	* Customer last name
 -- 	* ship date
 -- 	* book title
-
+-- SELECT c.first_name, c.last_name, s.ship_date, b.title FROM books b JOIN editions e ON b.id = e.book_id JOIN shipments s ON e.isbn = s.isbn JOIN customers c ON c.id = s.customer_id
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+-- SELECT COUNT(*) FROM books;
 -- 12. Get the COUNT of all Locations
+-- SELECT COUNT(locatiom) FROM subjects;
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+-- SELECT books.title COUNT(editions.edition)
+-- FROM books JOIN editions ON book.id = edition.book_id GROUP BY books.id;
