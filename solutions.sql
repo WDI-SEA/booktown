0efd5a4e0648e9c485f8c50ebe395261d2960e42
gracenarez333 <gracemnarez@gmail.com>
2022-05-17 10:14:54 -0700

    finishes deliverable

--- solutions.sql
+++ solutions.sql
@@ -162,6 +162,43 @@ booktown-# order by shipments.ship_date;
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
--- 12. Get the COUNT of all Locations
+booktown=# select count(*) from books;
+ count 
+-------
+    15
+-- 12. Get the COUNT of all 
+booktown=# select count(location) from subjects;
+ count 
+-------
+    15
+(1 row)
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+booktown=# select location, count(location) from subjects group by location;
+     location     | count 
+------------------+-------
+                  |     0
+ Sunset Dr        |     1
+ Kids Ct          |     1
+ Black Raven Dr   |     2
+ Creativity St    |     2
+ Academic Rd      |     2
+ Main St          |     4
+ Productivity Ave |     3
+(8 rows)
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+booktown=# booktown=# select books.id, books.title, count(editions.edition) from books                                                                                  join editions on books.id = editions.book_id                                        group by books.id, books.title;
+  id   |            title            | count 
+-------+-----------------------------+-------
+  2038 | Dynamic Anatomy             |     1
+ 25908 | Franklin in the Dark        |     1
+  7808 | The Shining                 |     2
+  4267 | 2001: A Space Odyssey       |     2
+ 41473 | Programming Python          |     1
+  1234 | The Velveteen Rabbit        |     1
+  4513 | Dune                        |     2
+  1608 | The Cat in the Hat          |     2
+   190 | Little Women                |     1
+  1501 | Goodnight Moon              |     1
+   156 | The Tell-Tale Heart         |     2
+  1590 | Bartholomew and the Oobleck |     1
+(12 rows)
