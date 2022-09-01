7873068825f1a6b01d1022849e98315d781a783d
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-09-01 11:57:42 -0400

    solutions

--- solutions.sql
+++ solutions.sql
@@ -1,24 +1,41 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+--Code: SELECT * FROM subjects;
+
 -- 2. Find all subjects sorted by location
+--Code: SELECT location FROM subjects;
+--Alternate: SELECT * FROM subjects ORDER BY location;
 
 -- ### Where
 -- 3. Find the book "Little Women"
--- 4. Find all books containing the word "Python"
+--Code: SELECT * FROM books WHERE title - 'Little Women';
+
+-- 4. Find all books containing the word 'Python'
+-- SELECT * FROM books WHERE title LIKE '%Python';
+
+
 -- 5. Find all subjects with the location "Main St" sort them by subject
+--SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+--SELECT books.title FROM books JOIN subjects ON books.subject_id = subjects.id WHERE subjects.id = 4
+
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+
+--SELECT books.title, authors.last_name, authors.first_name, subjects.subject FROM books JOIN authors ON books.author_id = authors.id JOIN subjects ON books.subject_id = subjects.id;
+
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+
+
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
