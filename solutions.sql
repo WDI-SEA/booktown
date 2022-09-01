5bbaf0d61ff7b9c50f38542765b50a6e33e08c2e
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-09-01 00:50:43 -0500

    Question 1-7 Doneothers needs checking

--- solutions.sql
+++ solutions.sql
@@ -1,21 +1,28 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+SELECT * FROM subjects ORDER BY subject;
 -- 2. Find all subjects sorted by location
+SELECT * FROM subjects ORDER BY location;
 
 -- ### Where
 -- 3. Find the book "Little Women"
+SELECT * FROM books WHERE title='Little Women';
 -- 4. Find all books containing the word "Python"
+SELECT * FROM books WHERE title LIKE '%Python%';
 -- 5. Find all subjects with the location "Main St" sort them by subject
-
+SELECT * FROM subjects WHERE location='Main St' ORDER BY subject;
 
 -- ### Joins
-
 -- 6. Find all books about Computers and list ONLY the book titles
+SELECT title FROM subjects JOIN books ON books.subject_id=subjects.id WHERE subjects.subject='Computers';
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+SELECT title,first_name,last_name,subject FROM books JOIN(
+   SELECT * FROM subjects JOIN authors ON subjects.id=authors.id
+)ON books.subject_id = subjects.id;
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
@@ -30,9 +37,14 @@
 -- 	* ship date
 -- 	* book title
 
+
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+SELECT COUNT(*) FROM books;
 -- 12. Get the COUNT of all Locations
+SELECT COUNT(DISTINCT location) FROM subjects;
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+SELECT COUNT(location), location FROM subjects GROUP BY location;
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
+SELECT id,title,COUNT(edition) FROM (SELECT * FROM books JOIN editions ON books.id=editions.book_id)  GROUP BY books.id;
