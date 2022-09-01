a9e231fae51393537db3ba4369865cac922da4e6
syorn96 <syorn2014@gmail.com>
2022-08-31 17:15:08 -0700

    completes question 6

--- solutions.sql
+++ solutions.sql
@@ -69,6 +69,34 @@ SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+SELECT * FROM subjects;
+ id |     subject      |     location     
+----+------------------+------------------
+  0 | Arts             | Creativity St
+  1 | Business         | Productivity Ave
+  2 | Children's Books | Kids Ct
+  3 | Classics         | Academic Rd
+  4 | Computers        | Productivity Ave
+  5 | Cooking          | Creativity St
+  6 | Drama            | Main St
+  7 | Entertainment    | Main St
+  8 | History          | Academic Rd
+  9 | Horror           | Black Raven Dr
+ 10 | Mystery          | Black Raven Dr
+ 11 | Poetry           | Sunset Dr
+ 12 | Religion         | 
+ 13 | Romance          | Main St
+ 14 | Science          | Productivity Ave
+ 15 | Science Fiction  | Main St
+
+SELECT title FROM books WHERE subject_id = 4;
+        title         
+----------------------
+ Programming Python
+ Learning Python
+ Perl Cookbook
+ Practical PostgreSQL
+
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
