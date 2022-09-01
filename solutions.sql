e1bbf3c8fd4a25c08874f7844d0a74e4f1888a44
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-31 19:39:18 -0700

    1-9 done forgot to commit i am sorry.

--- solutions.sql
+++ solutions.sql
@@ -1,29 +1,60 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+
+SELECT * FROM subjects ORDER BY subject;
+
 -- 2. Find all subjects sorted by location
+
+SELECT * FROM subjects ORDER BY location;
 
 -- ### Where
 -- 3. Find the book "Little Women"
+
+SELECT * FROM books WHERE title = 'Little Women';
+
 -- 4. Find all books containing the word "Python"
+
+SELECT * FROM books WHERE title  LIKE '%Py%';
+
 -- 5. Find all subjects with the location "Main St" sort them by subject
 
+SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+
+SELECT books.title FROM books
+JOIN subjects ON books.subject_id = subjects.id 
+WHERE subjects.id = 4;
+
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+
+SELECT books.title,authors.first_name, authors.last_name, subjects.subject FROM books
+JOIN authors ON books.author_id = authors.id
+JOIN subjects ON books.subject_id = subjects.id;
+
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+SELECT books.title, stock.retail FROM books
+JOIN editions ON editions.book_id = books.id
+JOIN stock ON editions.isbn = stock.isbn
+ORDER BY stock.retail DESC;
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
 -- 	* Publisher name
 -- 	* Retail price
+SELECT books.title, editions.isbn, publishers.name, stock.retail FROM books
+JOIN editions ON editions.book_id = books.id
+JOIN publishers ON  editions.Publisher_id = publishers.id
+JOIN stock ON editions.isbn = stock.isbn 
+WHERE books.title = 'Dune';
 -- 10. Find all shipments sorted by ship date display a result table with ONLY the following columns:
 -- 	* Customer first name
 -- 	* Customer last name
