c7c739a94e14f441ccc48229e9dff59ab6fb4306
tpalmer111D <timpalmer@Tims-MacBook-Pro.local>
2022-08-31 22:05:39 -0600

    booktown deliverable solutions, 1-9

--- solutions.sql
+++ solutions.sql
@@ -1,38 +1,102 @@
 -- ### Order
 -- 1. Find all subjects sorted by subject
+
+SELECT * FROM books ORDER BY subject_id;
+
 -- 2. Find all subjects sorted by location
+
+SELECT * FROM subjects ORDER BY subjects.location;
 
 -- ### Where
 -- 3. Find the book "Little Women"
+
+SELECT * FROM books WHERE title = 'Little Women';
+
 -- 4. Find all books containing the word "Python"
+
+SELECT * FROM books WHERE title LIKE '%Python%';
+
 -- 5. Find all subjects with the location "Main St" sort them by subject
 
+SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subjects.subject;
 
 -- ### Joins
 
 -- 6. Find all books about Computers and list ONLY the book titles
+
+SELECT books.title, subjects.id
+FROM books 
+INNER JOIN subjects 
+ON books.subject_id=subjects.id
+WHERE subjects.subject = 'Computers';
+
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
 -- 	* Author's first name
 -- 	* Author's last name
 -- 	* Book subject
+
+SELECT books.title, authors.first_name, authors.last_name, subjects.subject
+FROM books
+JOIN authors
+ON  books.author_id=authors.id
+JOIN subjects
+ON books.subject_id=subjects.id;
+
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
 -- 	* Display ONLY: title and price
+
+SELECT books.title, stock.retail
+FROM stock
+JOIN editions
+ON stock.isbn=editions.isbn
+JOIN books
+ON editions.book_id=books.id
+ORDER BY stock.retail DESC;
+
+-- SELECT books.title, stock.retail
+-- FROM books
+-- JOIN editions
+-- ON books.id=editions.book_id
+-- JOIN stock
+-- ON editions.isbn=stock.isbn
+-- ORDER BY stock.retail DESC;
+
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
 -- 	* ISBN number
 -- 	* Publisher name
 -- 	* Retail price
+
+SELECT books.title, editions.isbn, publishers.name, stock.retail
+FROM books
+JOIN editions
+ON books.id=editions.book_id
+JOIN publishers
+ON editions.publisher_id=publishers.id
+JOIN stock
+ON editions.isbn=stock.isbn
+WHERE title = 'Dune';
+
 -- 10. Find all shipments sorted by ship date display a result table with ONLY the following columns:
 -- 	* Customer first name
 -- 	* Customer last name
 -- 	* ship date
 -- 	* book title
 
+
+
 -- ### Grouping and Counting
 
 -- 11. Get the COUNT of all books
+
+
 -- 12. Get the COUNT of all Locations
+
+
 -- 13. Get the COUNT of each unique location in the subjects table. Display the count and the location name. (hint: requires GROUP BY).
+
+
+
 -- 14. List all books. Display the book_id, title, and a count of how many editions each book has. (hint: requires GROUP BY and JOIN)
