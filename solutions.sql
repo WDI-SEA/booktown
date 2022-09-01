e63b1fc5a2154169350f03e4a5e0ac67b76af0c6
average-alien <allentang113@gmail.com>
2022-09-01 09:52:46 -0700

    adds commands using aliases

--- solutions.sql
+++ solutions.sql
@@ -21,6 +21,10 @@ SELECT * FROM subjects WHERE location = 'Main St' ORDER BY subject;
 SELECT books.title FROM books
 JOIN subjects ON books.subject_id = subjects.id
 WHERE subjects.subject = 'Computers';
+-- using aliases
+SELECT b.title FROM books b
+JOIN subjects s ON b.subject_id = s.id
+WHERE s.subject = 'Computers';
 
 -- 7. Find all books and display a result table with ONLY the following columns
 -- 	* Book title
@@ -30,6 +34,10 @@ WHERE subjects.subject = 'Computers';
 SELECT books.title, authors.first_name, authors.last_name, subjects.subject FROM books
 JOIN authors ON books.author_id = authors.id
 JOIN subjects ON books.subject_id = subjects.id;
+-- using aliases
+SELECT b.title, a.first_name, a.last_name, s.subject FROM books b
+JOIN authors a ON b.author_id = a.id
+JOIN subjects s ON b.subject_id = s.id;
 
 -- 8. Find all books that are listed in the stock table
 -- 	* Sort them by retail price (most expensive first)
@@ -38,6 +46,11 @@ SELECT books.title, stock.retail FROM editions
 JOIN books ON editions.book_id = books.id
 JOIN stock ON editions.isbn = stock.isbn
 ORDER BY stock.retail DESC;
+-- using aliases
+SELECT b.title, s.retail FROM editions e
+JOIN books b ON e.book_id = b.id
+JOIN stock s ON e.isbn = s.isbn
+ORDER BY s.retail DESC;
 
 -- 9. Find the book "Dune" and display ONLY the following columns
 -- 	* Book title
@@ -49,6 +62,12 @@ JOIN books ON editions.book_id = books.id
 JOIN publishers ON editions.publisher_id = publishers.id
 JOIN stock ON editions.isbn = stock.isbn
 WHERE books.title = 'Dune';
+-- using aliases
+SELECT b.title, e.isbn, p.name, s.retail FROM editions e
+JOIN books b ON e.book_id = b.id
+JOIN publishers p ON e.publisher_id = p.id
+JOIN stock s ON e.isbn = s.isbn
+WHERE b.title = 'Dune';
 
 -- 10. Find all shipments sorted by ship date display a result table with ONLY the following columns:
 -- 	* Customer first name
@@ -60,6 +79,12 @@ JOIN editions ON shipments.isbn = editions.isbn
 JOIN books ON books.id = editions.book_id
 JOIN customers ON shipments.customer_id = customers.id
 ORDER BY shipments.ship_date;
+-- using aliases
+SELECT c.first_name, c.last_name, s.ship_date, b.title FROM shipments s
+JOIN editions e ON s.isbn = e.isbn
+JOIN books b ON b.id = e.book_id
+JOIN customers c ON s.customer_id = c.id
+ORDER BY s.ship_date;
 
 -- ### Grouping and Counting
 
@@ -77,3 +102,7 @@ SELECT books.id, books.title, COUNT(edition)
 FROM books
 FULL OUTER JOIN editions ON books.id = editions.book_id
 GROUP BY books.id;
+-- using aliases
+SELECT b.id, b.title, COUNT(edition) FROM books b
+FULL OUTER JOIN editions e ON b.id = e.book_id
+GROUP BY b.id;
