65d4f6c3cb2eb5812b373c614d174d09ba23d5ad
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-06 08:22:43 -0700

    update

---
+++ ql -f booktown.sql
@@ -0,0 +1,12 @@
+                                  List of databases
+    Name    |  Owner   | Encoding |   Collate   |    Ctype    |   Access privileges   
+------------+----------+----------+-------------+-------------+-----------------------
+ mcpeepants | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+ postgres   | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+ template0  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+            |          |          |             |             | postgres=CTc/postgres
+ template1  | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/postgres          +
+            |          |          |             |             | postgres=CTc/postgres
+ world      | postgres | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+(5 rows)
+
